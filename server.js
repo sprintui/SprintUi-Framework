0260aa4996f0468b9632fdbf2ab27ccd2b46f4d9
Aloysius Faustin <62741287+babymonie@users.noreply.github.com>
2024-03-20 21:12:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -270,7 +270,7 @@ app.listen(port, async () => {
 
   //check version file
 
-  if ((await getVersion(versionFileURL)) != sV) {
+   if (parseFloat(await getVersion(versionFileURL)) < parseFloat(sV)) {
     console.log(
       "\x1b[31m%s\x1b[0m",
       "Sprint UI is not up to date. Please update to the latest version"
